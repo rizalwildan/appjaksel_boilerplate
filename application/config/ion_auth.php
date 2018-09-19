721dd6d1e757c327bb4bbe381243ddfbe77d8676
rizalwildan <rizal.wildan.a@gmail.com>
2018-09-19 13:04:25 +0700

    initial commit

---
+++ application/config/ion_auth.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Created by PhpStorm.
+ * User: mrrobot
+ * Date: 19/09/18
+ * Time: 12:51
+ */
---
+++ application/models/Absen_model.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Created by PhpStorm.
+ * User: mrrobot
+ * Date: 19/09/18
+ * Time: 4:51
+ */
+
+class Absen_model extends yidas\Model
+{
+
+}
